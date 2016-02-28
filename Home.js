d698fac7e895f36f39c254296d780d042a2c6649
Clareesi <clarali2000@126.com>
2016-02-28 15:22:40 -0500

    lots

--- Home.js
+++ Home.js
@@ -6,7 +6,7 @@ $(window).resize(function(){
 		top: ($(window).height() - $('.circle').outerHeight())/2,
 	});
 
-	$('#circle2').css({
+	$('.circle2').css({
 		position:"absolute",
 		left: ($(window).width() - $('.circle').outerWidth())/2,
 		top: ($(window).height() - $('.circle').outerHeight())/2,
@@ -16,13 +16,31 @@ $(window).resize(function(){
 
 $(window).resize()
 
+// var line_angle = function(a, b){
+// 	var dy = a.position().top - b.position().top;
+// 	var dx = a.position().left - b.position().left;
+// 	return Math.arctan(dy/dx);
+// }
+
 $(document).ready(function(){
-	var circle2 = $("#circle2");
+	var circle2 = $(".circle2");
+	// $(".line_wrapper").position().top = circle2.position().top;
     $(".circle").hover(function(){
-        circle2.fadeIn();
-        circle2.css({
+        $("#archive").fadeIn();
+        $("#archive").css({
         	transform: 'translateY(-75px) translateX(-200px)',
         })
+        $("#random").fadeIn();
+        $("#random").css({
+        	transform: 'translateY(275px) translateX(50px)',
+        })
+        $("#about").fadeIn();
+        $("#about").css({
+        	transform: 'translateY(-200px) translateX(200px)',
+        })
+
+    // $(".line").animate({width:"15%"}, '5s');
+
 
     });
 });
