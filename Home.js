a4e6d1cfc3c317729d59249c91657e6ba8e89e42
Clareesi <clarali2000@126.com>
2016-02-28 14:06:15 -0500

    home

--- Home.js
+++ Home.js
@@ -6,10 +6,25 @@ $(window).resize(function(){
 		top: ($(window).height() - $('.circle').outerHeight())/2,
 	});
 
-	console.log($(window).height())
-	console.log($('.circle').outerHeight()/2)
-	console.log($(window).height() - $('.circle').outerHeight()/2)
+	$('#circle2').css({
+		position:"absolute",
+		left: ($(window).width() - $('.circle').outerWidth())/2,
+		top: ($(window).height() - $('.circle').outerHeight())/2,
+	});
 
 });
 
 $(window).resize()
+
+$(document).ready(function(){
+	var circle2 = $("#circle2");
+    $(".circle").hover(function(){
+        circle2.fadeIn();
+        circle2.css({
+        	transform: 'translateY(-75px) translateX(-200px)',
+        })
+
+    });
+});
+
+$(document).ready()
